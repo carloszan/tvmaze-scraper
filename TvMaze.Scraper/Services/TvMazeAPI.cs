992bca6411b55a8a135d6626c2d649010888d1c2
Carlos <carloshlzansavio@gmail.com>
2023-08-05 22:27:37 -0300

    Create projects

---
+++ TvMaze.Scraper/Services/TvMazeAPI.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+
+namespace TvMaze.Scraper.Services
+{
+    public class TvMazeAPI
+    {
+        private readonly ILogger<TvMazeAPI> _logger;
+        private readonly HttpClient _httpClient;
+
+        public TvMazeAPI(
+            ILogger<TvMazeAPI> logger,
+            HttpClient httpClient)
+        {
+            _logger = logger;
+            _httpClient = httpClient;
+        }
+
+        public async Task<int> GetLastShowPage()
+        {
+            int lastPage = 0;
+            var tasks = new List<Task>();
+
+            for (var i = lastPage; i < 500; i++)
+            {
+                tasks.Add(Task.Run(() =>
+                {
+                    var page = i;
+                    lastPage = Math.Max(lastPage, page);
+                }));
+            }
+            await Task.WhenAll(tasks);
+
+            return lastPage;
+        }
+    }
+}
---
+++ TvMaze.Tests/Services/TvMazeAPI_Tests.cs
@@ -0,0 +1,18 @@
+﻿using TvMaze.Scraper.Services;
+
+namespace TvMaze.Tests.Services
+{
+    [TestClass]
+    public class TvMazeAPI_Tests
+    {
+        [TestMethod]
+        public async Task TvMazeAPI_WithValidParameters_ReturnsLastShowPage()
+        {
+            var service = new TvMazeAPI();
+
+            var lastPage = await service.GetLastShowPage();
+
+            Assert.IsTrue(lastPage > 0);
+        }
+    }
+}
